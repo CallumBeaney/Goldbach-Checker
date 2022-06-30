01e14d3bbb90655ecccea223778fa98f7eec87b2
Callum Beaney <79003912+CallumBeaney@users.noreply.github.com>
2022-06-30 22:34:35 +0100

    fixed memory handling error

--- primefilters/primeLookupGeneratorVer1.c
+++ primefilters/primeLookupGeneratorVer1.c
@@ -1,3 +1,7 @@
+/* This program loads the result of a prime checking function as (1 = is prime | 0 = isn't prime) into a lookup table based on user input.
+Presently runs to around Five Hundred Million 500000000 but not to One Billion 1000000000.
+This model is not the most efficient; alternative sieves/wheel factorisation etc will be employed in future versions. */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,23 +10,29 @@ typedef uint32_t u32;
 u32 getInput(void);
 int isPrime(u32 n);
 char * populatePrimeLookup(u32 userInput);
-
-// TODO: Presently runs to around Five Hundred Million 500000000 but not to One Billion 1000000000.
+// void debugger(char *primeLookup);
 
 int main(void)
 {
     u32 userInput = getInput();
 
-    char * primeLookup;
-    primeLookup = populatePrimeLookup(userInput);
+    char * primeLookup = populatePrimeLookup(userInput);
+
+    // debugger(primeLookup);
+
+    /* primeLookup now points to space in memory malloc'd for passArray in below Fn */
+    free(primeLookup); 
+    
+    printf("Done.\n");
 }
 
 char * populatePrimeLookup(u32 userInput)
 {
     char *passArray = malloc(sizeof(userInput));  
+
     if(passArray == NULL)
     {
-        printf("Unable to check.\n");
+        printf("Unable to allocate memory.\n");
         exit(1);
     }
 
@@ -31,6 +41,7 @@ char * populatePrimeLookup(u32 userInput)
         char primeStatus = isPrime(i);
         passArray[i] = primeStatus;
     }
+
     return passArray;
 }
 
@@ -66,28 +77,21 @@ u32 getInput(void)
             exit(1);
         }
 
-        //  strtoull(converted buffer, string to catch non-numbers if needed, base 10)
+        /* strtoull(converted buffer, string to catch non-numbers if needed, base 10) */
         a = (uint32_t)strtoul(buf, &ptr, 10);
     
-        printf("Running...\n");
         return a;
 
     } while (a == 0);
 }
 
-    //     //DEBUG:
-    //     printf("\n\n\n0 is %i  and should be 0\n",  primeLookup[0]);
-    //     printf("1 is %i  and should be 0\n",  primeLookup[1]);
-    //     printf("2 is %i  and should be 1\n",  primeLookup[2]);
-    //     printf("3 is %i  and should be 1\n",  primeLookup[3]);
-    //     printf("4 is %i  and should be 0\n\n",  primeLookup[4]);
-    //     printf("29 is %i and should be 1\n\n", primeLookup[29]);
-    //     printf("96 is %i and should be 0\n", primeLookup[96]);
-    //     printf("97 is %i and should be 1\n", primeLookup[97]);
-    //     printf("98 is %i and should be 0\n", primeLookup[98]);
-    //     // printf("7919 is %i and should be 1 \n", primeLookup[7919]);
-    //     // printf("7920 is %i and should be 0\n", primeLookup[7920]);
-    //     // printf("999983 is %i and should be 1\n", primeLookup[999983]);
-    //     // printf("999985 is %i and should be 0\n", primeLookup[999985]);
-    //     // printf("111111100 is %i and should be 0\n\n", primeLookup[111111100]);
-    //     // printf("15485863 is %i and should be 1\n", primeLookup[15485863]);
+// void debugger(char* primeLookup)
+// {
+//         printf("1 = is prime; 0 = is not prime\n");
+//         printf("\n0 \tis %i and should be 0\n", primeLookup[0]);
+//         printf("1 \tis %i and should be 0\n",   primeLookup[1]);
+//         printf("2 \tis %i and should be 1\n",   primeLookup[2]);
+//         printf("3 \tis %i and should be 1\n",   primeLookup[3]);
+//         printf("4 \tis %i and should be 0\n\n", primeLookup[4]);
+//         printf("5 \tis %i and should be 1\n\n", primeLookup[5]);
+// }
